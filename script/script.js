683be74f0a1748a4416ac313ef1a43a73625b2a8
juyel66 <mdjuyelrana.com.bd1@gmail.com>
2024-02-19 11:28:55 +0600

    button click the scroll buyTicket Section

---
+++ script/script.js
@@ -0,0 +1,25 @@
+// function scrollDownToPHSection() {
+//     const phSection = document.getElementById('ph');
+//     if (phSection) {
+//         phSection.scrollIntoView({ behavior: 'smooth' });
+//     }
+// }
+
+// document.addEventListener('DOMContentLoaded', function() {
+//     const bannerButton = document.querySelector('.hero-content button');
+//     if (bannerButton) {
+//         bannerButton.addEventListener('click', scrollDownToPHSection);
+//     }
+// });
+
+
+
+// step-1: click the button then scroll buyTicket section 
+
+function buyTicket(){
+    const scrollButtonToBuyTicketSection =document.getElementById('buyTicket');
+    scrollButtonToBuyTicketSection.scrollIntoView({ behavior: 'smooth'});
+}
+
+
+
